a76e98d4e5669b07cc9fa4b5e2ab53079cbc65f9
Anna S <annasun.usa@gmail.com>
2019-06-04 00:24:30 +0800

    Pokedex Game

---
+++ Sec05-pokedex_project/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Pokegame from './Pokegame';
+//  import Pokedex from './Pokedex';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Pokedex /> */}
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
