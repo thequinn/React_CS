3add339c2af3722dc22d4d4f2840877cf4a10690
Anna S <annasun.usa@gmail.com>
2019-06-03 20:49:30 +0800

    First commit

---
+++ Sec07-Dice_Exercise/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Die from './Die';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Die />
+    </div>
+  );
+}
+
+export default App;
---
+++ Sec07-Dice_Exercise/src/Die.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Die extends Component {
+  render() {
+    return <h1>Die!!</h1>
+  }
+}
+
+
+export default Die;
