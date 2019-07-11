fc8d87457aa64333010962e2ab665d27d866469f
Anna S <annasun.usa@gmail.com>
2019-07-12 05:32:57 +0800

    Practiced Controlled component and Computed property names

--- Sec13-forms-in-react-90_92/controlled-components/src/Form.js
+++ Sec13-forms-in-react-90_92/controlled-components/src/Form.js
@@ -5,25 +5,35 @@ class Form extends Component {
     super(props);
     this.state = {username: ""};
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(evt) {
-    alert(`You types: ${this.state.username}`);
     this.setState( {username: evt.target.value} );
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    alert(`You typed: ${this.state.username}`);
+    this.setState( { username: "" } );
   }
 
   render() {
     return(
-      <form  >
-        <label htmlFor="username">Username</label>
-        <input 
-          type="text" 
-          id="username" 
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        <button>Submit</button>
-      </form>
+      <div>
+        <h1>Form Demo</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input 
+            type="text" 
+            id="username" 
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
     );
   }
 }
--- Sec13-forms-in-react-90_92/controlled-components/src/MultipleForm.js
+++ Sec13-forms-in-react-90_92/controlled-components/src/MultipleForm.js
@@ -1,8 +1,50 @@
 import React, { Component } from 'react'
 
 class MultipleForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", email: "", password: ""}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(evt) {
+    this.setState( { [evt.target.name]: evt.target.value } );
+  }
+  handleSubmit(evt) {
+    evt.preventDefault();
+    alert(`You typed: ${this.state.username}`);
+    this.setState( { [evt.target.name]: "" } );
+  }
   render() {
-    return(<div></div>);
+    return(
+      <div>
+        <h1>Form with Multiple Inputs</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            name="username" 
+            value={this.state.username} 
+            placeholder="username" 
+            onChange={this.handleChange} 
+          />
+          <input 
+            type="text" 
+            name="email" 
+            value={this.state.email} 
+            placeholder="email" 
+            onChange={this.handleChange} 
+          />
+          <input 
+            type="text" 
+            name="password" 
+            value={this.state.password} 
+            placeholder="password" 
+            onChange={this.handleChange} 
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
 
