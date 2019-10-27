efb42c2d7e6e039a8a9b59df7d4915d338af3212
thequinn <annasun.usa@gmail.com>
2019-10-27 09:40:42 +0800

    Update App.js

--- Sec36-State_Mgmt_w_useReducer_n_useContext/first-reducer/App.js
+++ Sec36-State_Mgmt_w_useReducer_n_useContext/first-reducer/App.js
@@ -1,9 +1,6 @@
-// Lecture ex: https://reactjs.org/docs/hooks-reference.html#usereducer
-
 import React, { useReducer } from 'react';
 import './App.css';
 
-// The reducer takes the "action" and then update the state.
 function reducer(state, action) {
   switch (action.type) {
     case 'increment':
@@ -18,11 +15,7 @@ function reducer(state, action) {
 }
 
 function App() {
-  // 技巧: useReducer() is an alternative to useState()
-  //
-  // dispatch() is a func returned by useReducer() that's generated base off of the reducer callback we passed in.  (簡單説:  dispatch() uses reducer callback to update the state)
   const [state, dispatch] = useReducer(reducer, {count: 0});
-
   return (
     <div className='App'>
        <h1>Count: {state.count}</h1>
