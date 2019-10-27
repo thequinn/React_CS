6701112b20b3a136b346c1d9478d8bfc46ea5714
thequinn <annasun.usa@gmail.com>
2019-10-28 04:55:59 +0800

    Update todos.context.js
    Update comments

--- Sec36-State_Mgmt_w_useReducer_n_useContext/2-add-first-context/src/context/todos.context.js
+++ Sec36-State_Mgmt_w_useReducer_n_useContext/2-add-first-context/src/context/todos.context.js
@@ -1,26 +1,15 @@
-import React, { createContext } from 'react';
+import React, { createContext } from "react";
 import useTodoState from "../hooks/useTodoState";
-
 const defaultTodos = [
   { id: 1, task: "Mow the lawn using goats", completed: false },
   { id: 2, task: "Release lady bugs into garden", completed: true }
 ];
-
-// Create a context obj
 export const TodosContext = createContext();
 
 export function TodosProvider(props) {
-
-  // Get the obj returned from useTodoState()
-  // - The obj returned is { todos, addTodo, removeTodo, toggleTodo, editTodo } 
-  // - The obj properties will be used as "value" prop ln-20 to be passed to consuming comps in ln-24
-  const todoStuff = useTodoState(defaultTodos);  
-
+  const todosStuff = useTodoState(defaultTodos);
   return (
-    <TodosContext.Provider value={todoStuff}>
-      {/* 
-      {this.props.children} equals to the "nested comps" passed into <TodosProvider> ln-29~32 in TodoApp.js.  And these nested/child comps  can access the "value" prop in the provider.  Think of this as a pattern.
-      */}
+    <TodosContext.Provider value={todosStuff}>
       {props.children}
     </TodosContext.Provider>
   );
