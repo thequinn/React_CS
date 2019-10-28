cdf7a28ed73287670efc9e5368320ecb38a9e373
thequinn <annasun.usa@gmail.com>
2019-10-29 01:05:52 +0800

    init

---
+++ Sec37-Next_JS/pages/about.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+const AboutPage = () => {
+  //console.log("***** RUNNING ABOUT COMPONENT *****");
+
+  return (
+    <div>
+      <Navbar />
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ Sec37-Next_JS/pages/index.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+const Index = () => {
+  //console.log("***** RUNNING INDEX COMPONENT");
+
+  return(
+    <div>
+      <Navbar />
+      <h1>Index Page</h1>
+    </div>
+  );
+};
+
+export default Index;
